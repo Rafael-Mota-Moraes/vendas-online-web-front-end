302dcca0c6cc59a56e71ff5a7bf999e8ec371503
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-05-21 18:57:06 -0300

    Background Image on login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import ComponenteProprio from "./components/ComponenteProprio";
-import Contador from "./components/Contador";
-import TestCallback from "./components/TestCallback";
-import TestMemo from "./components/TestMemo";
+import LoginScreen from "./modules/login";
 
 function App() {
   return (
     <>
-      <div>
-        <ComponenteProprio
-          corBg="#ddd"
-          corFonte="#333"
-          textoCorpo="texto do corpo do componente"
-          titulo="Título do componente"
-        />
-        <hr />
-        <Contador />
-        <hr />
-        <TestCallback />
-        <hr />
-        <TestMemo />
-      </div>
+      <LoginScreen />
     </>
   );
 }
