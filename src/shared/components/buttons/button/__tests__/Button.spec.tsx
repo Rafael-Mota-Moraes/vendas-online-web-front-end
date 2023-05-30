6ed2f6922477fbf84b820afe045b4351b06088c3
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-05-30 11:15:27 -0300

    Configurando testes

---
+++ src/shared/components/buttons/button/__tests__/Button.spec.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import Button from "../Button";
+
+describe("Test Button", () => {
+  it("teste render", () => {
+    render(<Button />);
+  });
+});
